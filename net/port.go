f4fb2bc9adde3b001bfdae12c72902948fbec75f
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-04-15 14:40:21 -0600

    Escaneador de puertos con concurrencia

--- net/port.go
+++ net/port.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"sync"
 )
 
+var site = flag.String("site", "scanme.nmap.org", "url to scan") // Con flag decimos que sitio queremos escanear.
+
 func main() {
-	for i := 0; i < 100; i++ {
-		// 1, 2, ..4, 99
-		// sitio:1, sitio:2, sitio:99, ...,
-		// 1 -> Open, 2 -> Closed, ...
-		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i)) // Usamos tcp para conectarnos a esa red.
-		if err != nil {
-			continue
-		}
-		conn.Close()
-		fmt.Printf("Port %d is open\n", i)
+	flag.Parse()
+	var wg sync.WaitGroup
+	for i := 0; i < 65535; i++ {
+		wg.Add(1)
+		go func(port int) {
+			wg.Done()
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *site, port))
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("Port %d is open\n", port)
+		}(i)
 	}
+	wg.Wait() // El wait debe de estar fuera del ciclo for porque si esta dentro se bloqueara de 1 en 1.
 }
